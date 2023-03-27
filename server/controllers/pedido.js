cb30f9a69bd51c3fcb2a16d1ab26e2f47989157d
Lordsoro <98828393+Lordsoro@users.noreply.github.com>
2023-03-27 21:02:03 +0200

    Add files via upload

--- server/controllers/pedido.js
+++ server/controllers/pedido.js
@@ -1,11 +1,10 @@
-const Pedido = require('../models/pedido');
+const { Pedido } = require('../models/pedido');
 const Product = require('../models/product');
 
 async function register(req, res) {
-    const { user_id, products, quantity, size = 's' } = req.body;
+    const { user_id, products, quantity, total, size = 's' } = req.body;
     try {
         const productDocs = await Product.find({ _id: { $in: products } })
-        const total = productDocs.reduce((total, product) => total + (product.price * product.quantity), 0)
         const newPedido = new Pedido({
             user_id,
             products: productDocs,
