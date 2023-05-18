9173bc8daa1fe3e01cea402a6ad650b4d7df2bdb
Lordsoro <98828393+Lordsoro@users.noreply.github.com>
2023-05-18 13:50:19 +0200

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/user');
 const api = express.Router();
 
 api.get('/profile/:userId', userController.profile);
+api.delete('/profile/:userId', userController.deleteAccount);
 api.post('/register', userController.register);
 api.post('/login', userController.login);
 
