6e0732d27acb1198ac301b597fd17186fa77e30c
Lordsoro <98828393+Lordsoro@users.noreply.github.com>
2023-05-19 08:15:07 +0200

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,6 +5,7 @@ import Invierno from './Invierno';
 import Verano from './Verano';
 import DetallesInf from '../components/DetallesInf';
 import Navbar from '../components/Navbar';
+import Profile from './Profile';
 import Login from './Login';
 import Signin from './Signin';
 import Cart from './Cart'
@@ -23,13 +24,11 @@ export default function Home() {
                 <Routes>
                     <Route path='/' element={<Index />} />
                     <Route path='/invierno' element={<Invierno />} />
-                    <Route path='/verano' element={<Verano />} >
-                        <Route path='detalles' element={<DetallesInf />} />
-                    </Route>
+                    <Route path='/verano' element={<Verano />} />
                     <Route path='/cart' element={<Cart />} />
                     <Route path='/login' element={<Login />} />
                     <Route path='/register' element={<Signin />} />
-
+                    <Route path= '/profile' element={<Profile />} />  
                 </Routes>
                 <Footer />
             </CartContextProvider>
